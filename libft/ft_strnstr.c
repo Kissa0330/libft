ae641dfc1afa1e9bedc23ff5089de9f12aceb814
らいか <raika4789@gmail.com>
2022-04-11 01:42:26 +0900

    libft.h norm

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -12,24 +12,24 @@
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *hay, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
 	if (*needle == 0)
-		return ((char *)haystack);
-	while (haystack[i] != '\0' && i < len)
+		return ((char *)hay);
+	while (hay[i] != '\0' && i < len)
 	{
-		if (haystack[i] == needle[0])
+		if (hay[i] == needle[0])
 		{
 			j = 0;
-			while (haystack[i + j] == needle[j])
+			while (hay[i + j] == needle[j])
 			{
 				j++;
 				if (needle[j] == '\0')
-					return ((char *)haystack + i);
+					return ((char *)hay + i);
 			}
 		}
 		i++;
