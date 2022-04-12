82b5888404e6df5683121aaf2b324f4c710cb51a
らいか <raika4789@gmail.com>
2022-04-13 01:25:28 +0900

    fix ft_strrchr

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -14,24 +14,16 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
+	int		i;
 	char	*t;
-	char	*ret;
-	int		flag;
 
 	t = (char *)s;
-	ret = t;
-	while (*t != '\0')
+	i = ft_strlen(t);
+	while (i >= 0)
 	{
-		if (*t == c)
-		{
-			ret = t;
-			flag = 1;
-		}
-		t++;
+		if (t[i] == c)
+			return(&t[i]);
+		i --;
 	}
-	if (c == '\0')
-		return (t);
-	if (*t == '\0' && flag != 1)
-		return (NULL);
-	return (ret);
+	return (NULL);
 }
