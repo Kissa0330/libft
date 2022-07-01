4733d54496065484fb88903ab2cbf14c61d748fc
らいか <raika4789@gmail.com>
2022-07-01 12:52:53 +0900

    refactaring ft_strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -12,26 +12,11 @@
 
 #include "libft.h"
 
-char	*strjoin(char *s1, char *s2)
-{
-	size_t	length;
-	size_t	i;
-	char	*str;
-
-	i = 0;
-	length = ft_strlen(s1) + ft_strlen(s2) + 1;
-	str = malloc(sizeof(char) * length);
-	if (str == NULL)
-		return (str);
-	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
-	ft_strlcat(str, s2, length);
-	return (str);
-}
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str1;
-	char	*str2;
+	size_t	length;
+	char	*str;
 
 	if (s1 == NULL && s2 == NULL)
 		return (ft_strdup(""));
@@ -39,7 +24,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (ft_strdup(s2));
 	if (s2 == NULL)
 		return (ft_strdup(s1));
-	str1 = (char *)s1;
-	str2 = (char *)s2;
-	return (strjoin(str1, str2));
+	length = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = malloc(sizeof(char) * length);
+	if (str == NULL)
+		return (str);
+	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
+	ft_strlcat(str, s2, length);
+	return (str);
 }
